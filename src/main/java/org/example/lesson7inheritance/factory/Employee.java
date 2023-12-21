d0c3245b0c84c472b25b16140a0fa48004b8e086
Olga Tysevich <Olga.Tisevich@gmail.com>
2023-12-21 23:16:20 +0300

    added lesson 11

--- src/main/java/org/example/lesson7inheritance/factory/Employee.java
+++ src/main/java/org/example/lesson7inheritance/factory/Employee.java
@@ -1,7 +1,5 @@
 package org.example.lesson7inheritance.factory;
 
-/*Создать цепочку наследования, описывающую должностную структуру завода.
-* Начислить зарплату в зависимости от должности*/
 public abstract class Employee {
     private String department;
 
--- src/main/java/org/example/lesson7inheritance/factory/EmployeeDemo.java
+++ src/main/java/org/example/lesson7inheritance/factory/EmployeeDemo.java
@@ -3,7 +3,10 @@ package org.example.lesson7inheritance.factory;
 import java.util.ArrayList;
 import java.util.List;
 
+/*Создать цепочку наследования, описывающую должностную структуру завода.
+ * Начислить зарплату в зависимости от должности*/
 public class EmployeeDemo {
+
     public static void main(String[] args) {
         List<Employee> employees = new ArrayList<>();
         employees.add(new ChiefEngineer());
