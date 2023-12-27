c8cf2bc13f8a0f8b1ad0ed49b681a8a92d442902
Olga Tysevich <Olga.Tisevich@gmail.com>
2023-12-27 15:47:25 +0300

    updated task1 lesson 15

--- src/main/java/org/example/lesson15streams/Main.java
+++ src/main/java/org/example/lesson15streams/Main.java
@@ -1,20 +1,28 @@
 package org.example.lesson15streams;
 
+import java.util.function.IntPredicate;
 import java.util.stream.IntStream;
 
 public class Main {
+    public static final double CM_IN_INCH = 2.54;
+    public static final int MIN_INCH_VALUE = 1;
+    public static final int MAX_INCH_VALUE = 20;
+    public static final IntPredicate EVEN_NUMBER = n -> n % 2 == 0;
+
     public static void main(String[] args) {
         /*Напишите программу, которая для всех четных значений длинной от 1 до 20 дюймов:
         * печатает на экран значения в дюймах
         * переводит значения в сантиметры
         * печатает на экран сумму в сантиметрах*/
 
-        double sum = IntStream.rangeClosed(1, 20)
-                .filter(n -> n % 2 == 0)
+        double sum = IntStream.rangeClosed(MIN_INCH_VALUE, MAX_INCH_VALUE)
+                .filter(EVEN_NUMBER)
                 .peek(n -> System.out.print(n + " inch - "))
-                .mapToDouble(d -> d * 2.54)
+                .mapToDouble(inch -> inch * CM_IN_INCH)
                 .peek(n -> System.out.println(n + " cm"))
                 .sum();
         System.out.println("Sum: " + sum + " cm");
+
+
     }
 }
