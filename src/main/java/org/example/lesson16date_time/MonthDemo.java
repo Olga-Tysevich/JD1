7bcad865e288db6d31b0d65b993c2c231d3741a3
Olga Tysevich <Olga.Tisevich@gmail.com>
2023-12-27 22:58:07 +0300

    added task1 lesson16

---
+++ src/main/java/org/example/lesson16date_time/MonthDemo.java
@@ -0,0 +1,38 @@
+package org.example.lesson16date_time;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class MonthDemo {
+    public static final String DATE_PATTERN = "d::MMM::uuuu";
+    public static final int FIRST_DAY_OF_MONTH = 1;
+    public static final int MIN_MONTH_NUMBER = 1;
+    public static final int MAX_MONTH_NUMBER = 12;
+    public static void main(String[] args) {
+        /*Ввести с клавиатуры номер месяца текущего года.
+        Вывести на экран все его даты в формате d::MMM::uuuu*/
+       printAllDateOfMonth();
+    }
+
+    public static void printAllDateOfMonth() {
+        int monthNumber = getMonthNumber();
+        LocalDate date = LocalDate.now().withMonth(monthNumber).withDayOfMonth(FIRST_DAY_OF_MONTH);
+        do {
+            System.out.println(date.format(DateTimeFormatter.ofPattern(DATE_PATTERN)));
+            date = date.plusDays(1);
+        } while (date.getMonth().getValue() == monthNumber);
+    }
+
+    private static int getMonthNumber() {
+        Scanner scanner = new Scanner(System.in);
+        int monthNumber;
+        do {
+            System.out.println("Input month number: ");
+            monthNumber = scanner.hasNextInt() ? scanner.nextInt() : -1;
+            scanner.nextLine();
+        } while (monthNumber < MIN_MONTH_NUMBER || monthNumber > MAX_MONTH_NUMBER);
+
+        return monthNumber;
+    }
+}
