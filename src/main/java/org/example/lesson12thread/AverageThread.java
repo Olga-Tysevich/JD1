b904c0a4daee8f347311c920b8c7601bc1ec134d
Olga Tysevich <Olga.Tisevich@gmail.com>
2023-12-29 18:12:43 +0300

    updated lesson12

--- src/main/java/org/example/lesson12thread/AverageThread.java
+++ src/main/java/org/example/lesson12thread/AverageThread.java
@@ -1,15 +1,18 @@
 package org.example.lesson12thread;
 
 import java.util.List;
-public class AverageThread extends Thread{
+
+public class AverageThread extends Thread {
     private List<Integer> numbers;
 
     public AverageThread(List<Integer> numbers) {
         this.numbers = numbers;
     }
+
     @Override
     public void run() {
         System.out.println(Thread.currentThread().getName() + ":\n" + numbers);
-        System.out.println(Thread.currentThread().getName() +", average: " + (double) numbers.stream().reduce(Integer::sum).orElse(-1) / numbers.size());
+        System.out.println(Thread.currentThread().getName() + ", average: "
+                + (double) numbers.stream().reduce(Integer::sum).orElse(-1) / numbers.size());
     }
 }
--- src/main/java/org/example/lesson12thread/MaxThread.java
+++ src/main/java/org/example/lesson12thread/MaxThread.java
@@ -14,6 +14,7 @@ public class MaxThread extends Thread{
     @Override
     public void run() {
         intArray = new Random().ints(arraySize, 0, 101).toArray();
-        System.out.println(Thread.currentThread().getName() + ": " + Arrays.toString(intArray) + ", max: " + Arrays.stream(intArray).max().orElse(-1));
+        System.out.println(Thread.currentThread().getName() + ": " + Arrays.toString(intArray)
+                + ", max: " + Arrays.stream(intArray).max().orElse(-1));
     }
 }
