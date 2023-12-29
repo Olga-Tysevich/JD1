1e4322ef4f9678100e74e747f1743a76e1b12d9a
Olga Tysevich <Olga.Tisevich@gmail.com>
2023-12-29 18:19:32 +0300

    updated lesson14

--- src/main/java/org/example/lesson14executors/task59/Constants.java
+++ src/main/java/org/example/lesson14executors/task59/Constants.java
@@ -3,14 +3,14 @@ package org.example.lesson14executors.task59;
 public class Constants {
 
     //StringFileGenerator
-    public static final int NUMBER_OF_TASKS = 10;
-    public static final int NUMBER_OF_THREADS = 3;
-    public static final String OUT_DIRECTORY = "src/main/java/org/example/lesson14executors/task59/files/";
-    public static final String OUT_FILE_NAME = "$1-file$2.txt";
-    public static final String THREAD_NAME_MARKER = "$1";
-    public static final String FILE_NUMBER_MARKER = "$2";
-    public static final int NUMBER_OF_FILES = 10;
-    public static final int NUMBER_OF_STRINGS = 10;
-    public static final int MAX_SLEEP_TIME = 3000;
+    protected static final int NUMBER_OF_TASKS = 10;
+    protected static final int NUMBER_OF_THREADS = 3;
+    protected static final String OUT_DIRECTORY = "src/main/java/org/example/lesson14executors/task59/files/";
+    protected static final String OUT_FILE_NAME = "$1-file$2.txt";
+    protected static final String THREAD_NAME_MARKER = "$1";
+    protected static final String FILE_NUMBER_MARKER = "$2";
+    protected static final int NUMBER_OF_FILES = 10;
+    protected static final int NUMBER_OF_STRINGS = 10;
+    protected static final int MAX_SLEEP_TIME = 3000;
 
 }
