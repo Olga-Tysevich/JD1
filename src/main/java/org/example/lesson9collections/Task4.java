10665771205b2df95769501507ca113d9e1cc5c2
Olga Tysevich <Olga.Tisevich@gmail.com>
2023-12-29 17:56:47 +0300

    updated lesson9

--- src/main/java/org/example/lesson9collections/Task4.java
+++ src/main/java/org/example/lesson9collections/Task4.java
@@ -5,6 +5,9 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Task4 {
+    public static final String ANY_WORD = "[a-zA-Z]+|[а-яА-я]+";
+    public static final String NOT_WORD = "\\s+\\p{P}*\\s*|\\p{P}*\\n+\\p{P}*|\\s*\\p{P}+\\s*";
+
     public static void main(String[] args) {
         /*Ввести текст с клавиатуры. Для текста создать Мар, где ключом будет слово,
         а значение - количество повторений этого слова в тексте*/
@@ -12,7 +15,7 @@ public class Task4 {
         System.out.println(text);
 
         Map<String, Integer> wordsMap = new LinkedHashMap<>();
-        String[] words = text.split("\\s+\\p{P}*\\s*|\\p{P}*\\n+\\p{P}*|\\s*\\p{P}+\\s*");
+        String[] words = text.split(NOT_WORD);
 
         for (String w : words) {
             wordsMap.put(w, wordsMap.containsKey(w) ? wordsMap.get(w) + 1 : 1);
@@ -22,7 +25,7 @@ public class Task4 {
 
 
         wordsMap = new LinkedHashMap<>();
-        Pattern pattern = Pattern.compile("[a-zA-Z]+|[а-яА-я]+");
+        Pattern pattern = Pattern.compile(ANY_WORD);
         Matcher matcher = pattern.matcher(text);
         while (matcher.find()) {
             wordsMap.put(matcher.group(), wordsMap.containsKey(matcher.group()) ? wordsMap.get(matcher.group()) + 1 : 1);
